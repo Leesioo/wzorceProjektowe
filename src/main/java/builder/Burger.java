7f2aff2eaba9eaacefd21169ab8ad8d2cd773a9b
Leesioo <filip.lisiakiewicz@gmail.com>
2018-04-22 13:28:13 +0200

    wzorce projektowe - burger builder

--- src/main/java/builder/Burger.java
+++ src/main/java/builder/Burger.java
@@ -1,12 +1,71 @@
 package builder;
 
 public class Burger {
-    private BreadType type;
-    private String rodzajMiesa;
-    private String rodzajSalaty;
-    private String rodzajSera;
-    private String rodzajSosy;
+    private final BreadType type;
+    private final String rodzajMiesa;
+    private final String rodzajSalaty;
+    private final String rodzajSera;
+    private final String rodzajSosy;
     private boolean zawieraBekon;
+
+    public static class Builder {
+
+        private BreadType type;
+        private String rodzajMiesa;
+        private String rodzajSalaty;
+        private String rodzajSera;
+        private String rodzajSosy;
+        private boolean zawieraBekon;
+
+        public Builder() {
+        }
+
+        public Burger build() {
+            return new Burger(this);
+        }
+
+        public Builder zawieraBekon(boolean zawieraBekon) {
+            this.zawieraBekon = zawieraBekon;
+            return this;
+        }
+
+        public Builder type(BreadType type) {
+            this.type = type;
+            return this;
+        }
+        public Builder rodzajMiesa(String rodzajMiesa) {
+            this.rodzajMiesa = rodzajMiesa;
+            return this;
+        }
+
+        public Builder rodzajSalaty(String rodzajSalaty) {
+            this.rodzajSalaty = rodzajSalaty;
+            return this;
+        }
+
+        public Builder rodzajSera(String rodzajSera) {
+            this.rodzajSera = rodzajSera;
+            return this;
+        }
+
+        public Builder rodzajSosy(String rodzajSosy) {
+            this.rodzajSosy = rodzajSosy;
+            return this;
+        }
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    private Burger(Builder builder) {
+        this.type = builder.type;
+        this.rodzajMiesa = builder.rodzajMiesa;
+        this.rodzajSalaty = builder.rodzajSalaty;
+        this.rodzajSera = builder.rodzajSera;
+        this.rodzajSosy = builder.rodzajSosy;
+        this.zawieraBekon = builder.zawieraBekon;
+    }
 
     @Override
     public String toString() {
